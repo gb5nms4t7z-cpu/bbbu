8a8d1269c815a810d4926e0d4563a6659fbc90f3
gb5nms4t7z-cpu <gb5nms4t7z@privaterelay.appleid.com>
2026-02-18 14:54:28 +0200

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -4,6 +4,14 @@ print(utils.factorial(8))
 print(utils.is_prime(3))
 print(utils.fibonacci(3))
 print(utils.is_power_of_five(5))
-utils.helloworld()
-utils.sum(12,7)
+
+
+utils.helloworld()  
+
+print("Hello World")
+
+
+print(utils.sum(12, 7))
+
+
 print(utils.sum_digits(12345))
